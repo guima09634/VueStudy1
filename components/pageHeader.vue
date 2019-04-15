fbd653e3ec9a9e9a4127a047d134f4bd9e109e0e
Daniel <danielzheng@efun.com>
2019-04-15 18:52:09 +0800

    修改默认布局文件

---
+++ components/pageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="control-bar clearfix">
+    <div class="header-line pos-rel display-flex">
+      <div v-for="(item, index) in menuItem" :key="item.index">
+        <i :class="item.icon"></i>
+        <el-menu-item :index="index">{{ item.name }}</el-menu-item>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    menuItem:{
+      type:Array
+    }
+  }
+}
+</script>
---
+++ components/projectMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <transition name="el-zoom-in-top">
+    <el-aside class="side-bar" width="250px">
+      <div class="sider-header">
+        <span class="el-dropdown-link choise-item font-bold">小狐狸游戏管理后台</span>
+      </div>
+    </el-aside>
+    <MyMenu :menuTitles="menuList"/>
+  </transition>
+</template>
+
+<script>
+import MyMenu from './myMenu.vue'
+
+export default {
+  props: {
+    menuList: {
+      type: Array
+    }
+  }
+}
+</script>
+
